f91cb0a0b58c0b8535a446bfc746f63b7379f147
Kaushik Mehta <kaushikmehta30@gmail.com>
2020-08-04 18:49:18 -0400

    adds Confirm

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,7 @@ import Appointment from "components/Appointment/index"
 import Header from "components/Appointment/Header"
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
 
 storiesOf("Button", module)
   .addParameters({
@@ -141,3 +142,4 @@ storiesOf("Appointment", module)
   .add("Header", () => <Header time="12pm"/>)
   .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
   .add("Show", () => <Show student="Lydia Miller-Jones" interviewer={{ id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" }} onEdit={action("onEdit")} onDelete={action("onDelete")} />) 
+  .add("Confirm", () => <Confirm message="Delete the appointment?" onConfirm={action("onConfirm")} onCancel={action("onCancel")} />)
