868feb4f0d79393523f0afd9668d6c0e909560d3
Kaushik Mehta <kaushikmehta30@gmail.com>
2020-08-05 18:22:27 -0400

    uses axios to get data from API

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,26 +1,10 @@
 import React, { useState, useEffect } from "react";
+import axios from 'axios'
+
 
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = [
   {
@@ -70,7 +54,15 @@ const appointments = [
 ];
 
 export default function Application(props) {
+  const[days, setDays] = useState([]);
   const [day, setDay] = useState("Monday");
+
+  useEffect(()=> {
+    axios.get("http://localhost:8001/api/days")
+    .then((response)=> {
+      setDays(response.data);
+    })
+  }, [])
 
   const parsedAppointments = appointments.map(appointment => (
     <Appointment key={appointment.id} {...appointment} />))
