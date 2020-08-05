884498ecc04a5a34a04555d5c9678206ed5f19bb
Kaushik Mehta <kaushikmehta30@gmail.com>
2020-08-05 18:16:21 -0400

    adds Appointments to app

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,7 +1,8 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 
 import "components/Application.scss";
 import DayList from "./DayList";
+import Appointment from "./Appointment";
 
 const days = [
   {
@@ -21,8 +22,59 @@ const days = [
   },
 ];
 
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      },
+    },
+  },
+  {
+    id: 3,
+    time: "3pm",
+    interview: {
+      student: "Kaushik Mehta",
+      interviewer: {
+        id: 3,
+        name: "Mildred Nazir",
+        avatar: "https://i.imgur.com/T2WwVfS.png",
+      },
+    },
+  },
+  {
+    id: 4,
+    time: "4pm",
+  },
+  {
+    id: 5,
+    time: "5pm",
+    interview: {
+      student: "Notevena Student",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      },
+    },
+  },
+];
+
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+
+  const parsedAppointments = appointments.map(appointment => (
+    <Appointment key={appointment.id} {...appointment} />))
+  parsedAppointments.push(<Appointment key="last" time="6pm" />)
 
   return (
     <main className="layout">
@@ -35,9 +87,9 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-          days={days}
-          day={day}
-          setDay={setDay} />
+            days={days}
+            day={day}
+            setDay={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
@@ -46,7 +98,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {parsedAppointments}
       </section>
     </main>
   );
