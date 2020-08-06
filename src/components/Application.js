4e811e933c09ab909b5b449ebda79433a298c8f8
Kaushik Mehta <kaushikmehta30@gmail.com>
2020-08-06 13:38:31 -0400

    gets interviewer data from api and creates interview obj

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,38 +5,46 @@ import axios from 'axios'
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
-import { getAppointmentsForDay } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 export default function Application(props) {
-  // const[days, setDays] = useState([]);
-  // const [day, setDay] = useState("Monday");
-  // const [appointments, setAppointments] = useState([]);
-  let appointments={};
-  const [state, setState]=useState({
-    day:"Monday",
+  const [state, setState] = useState({
+    day: "Monday",
     days: [],
-    appointments:[]
+    appointments: {},
+    interviewers: {}
   });
 
-  useEffect(()=> {
+  useEffect(() => {
     Promise.all([
       axios.get("http://localhost:8001/api/days"),
       axios.get("http://localhost:8001/api/appointments"),
-
+      axios.get("http://localhost:8001/api/interviewers"),
     ])
-    
-    .then((all)=> {
-      console.log(all)
-      setState(prev => ({...prev, days:all[0].data, appointments: all[1].data}));
-    })
+
+      .then((all) => {
+        setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
+      })
   }, [])
 
   const fetchedAppointments = getAppointmentsForDay(state, state.day);
-  console.log("app here", fetchedAppointments)
-  
-  const parsedAppointments = fetchedAppointments.map(appointment => (
-    <Appointment key={appointment.id} {...appointment} />))
-  parsedAppointments.push(<Appointment key="last" time="6pm" />)
+  const parsedAppointments = fetchedAppointments.map(appointment => {
+    const interview = getInterview(state, appointment.interview);
+    return (
+      <Appointment
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
+      />
+    )
+
+  })
+
+
+  parsedAppointments.push(<Appointment key="last" time="5pm" />)
+
+
 
   return (
     <main className="layout">
@@ -52,7 +60,7 @@ export default function Application(props) {
             days={state.days}
             day={state.day}
             state={state}
-            setState={setState} /> 
+            setState={setState} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,14 +1,238 @@
- export function getAppointmentsForDay(state, day) {
+export function getAppointmentsForDay(state, day) {
   const filteredDay = state.days.filter(oneDay => oneDay.name === day);
-  let appointmentsArray=[];
+  let appointmentsArray = [];
   let results = [];
 
- if (filteredDay[0] && filteredDay[0].appointments){
-   appointmentsArray = filteredDay[0].appointments
-    for (const appointmentNumber of appointmentsArray){
+  if (filteredDay[0] && filteredDay[0].appointments) {
+    appointmentsArray = filteredDay[0].appointments
+    for (const appointmentNumber of appointmentsArray) {
       results.push(state.appointments[appointmentNumber])
     }
- }
+  }
 
-return results
+  return results
+}
+
+// const state = {
+//   "day": "Monday",
+//   "days": [{
+//       "id": 1,
+//       "name": "Monday",
+//       "appointments": [1, 2, 3, 4, 5],
+//       "interviewers": [2, 3, 5, 9, 10],
+//       "spots": 1
+//   }, {
+//       "id": 2,
+//       "name": "Tuesday",
+//       "appointments": [6, 7, 8, 9, 10],
+//       "interviewers": [1, 3, 5, 8, 10],
+//       "spots": 4
+//   }, {
+//       "id": 3,
+//       "name": "Wednesday",
+//       "appointments": [11, 12, 13, 14, 15],
+//       "interviewers": [1, 2, 5, 6, 10],
+//       "spots": 2
+//   }, {
+//       "id": 4,
+//       "name": "Thursday",
+//       "appointments": [16, 17, 18, 19, 20],
+//       "interviewers": [1, 2, 3, 7, 10],
+//       "spots": 4
+//   }, {
+//       "id": 5,
+//       "name": "Friday",
+//       "appointments": [21, 22, 23, 24, 25],
+//       "interviewers": [1, 4, 7, 8, 9],
+//       "spots": 4
+//   }],
+//   "appointments": {
+//       "1": {
+//           "id": 1,
+//           "time": "12pm",
+//           "interview": null
+//       },
+//       "2": {
+//           "id": 2,
+//           "time": "1pm",
+//           "interview": {
+//               "student": "Archie Cohen",
+//               "interviewer": 2
+//           }
+//       },
+//       "3": {
+//           "id": 3,
+//           "time": "2pm",
+//           "interview": {
+//               "student": "Chad Takahashi",
+//               "interviewer": 2
+//           }
+//       },
+//       "4": {
+//           "id": 4,
+//           "time": "3pm",
+//           "interview": {
+//               "student": "Jamal Jordan",
+//               "interviewer": 3
+//           }
+//       },
+//       "5": {
+//           "id": 5,
+//           "time": "4pm",
+//           "interview": {
+//               "student": "Leopold Silvers",
+//               "interviewer": 10
+//           }
+//       },
+//       "6": {
+//           "id": 6,
+//           "time": "12pm",
+//           "interview": null
+//       },
+//       "7": {
+//           "id": 7,
+//           "time": "1pm",
+//           "interview": null
+//       },
+//       "8": {
+//           "id": 8,
+//           "time": "2pm",
+//           "interview": null
+//       },
+//       "9": {
+//           "id": 9,
+//           "time": "3pm",
+//           "interview": null
+//       },
+//       "10": {
+//           "id": 10,
+//           "time": "4pm",
+//           "interview": {
+//               "student": "Liam Martinez",
+//               "interviewer": 3
+//           }
+//       },
+//       "11": {
+//           "id": 11,
+//           "time": "12pm",
+//           "interview": null
+//       },
+//       "12": {
+//           "id": 12,
+//           "time": "1pm",
+//           "interview": null
+//       },
+//       "13": {
+//           "id": 13,
+//           "time": "2pm",
+//           "interview": {
+//               "student": "Lydia Miller-Jones",
+//               "interviewer": 1
+//           }
+//       },
+//       "14": {
+//           "id": 14,
+//           "time": "3pm",
+//           "interview": {
+//               "student": "Maria Boucher",
+//               "interviewer": 2
+//           }
+//       },
+//       "15": {
+//           "id": 15,
+//           "time": "4pm",
+//           "interview": {
+//               "student": "Michael Chan-Montoya",
+//               "interviewer": 10
+//           }
+//       },
+//       "16": {
+//           "id": 16,
+//           "time": "12pm",
+//           "interview": null
+//       },
+//       "17": {
+//           "id": 17,
+//           "time": "1pm",
+//           "interview": null
+//       },
+//       "18": {
+//           "id": 18,
+//           "time": "2pm",
+//           "interview": {
+//               "student": "Richard Wong",
+//               "interviewer": 7
+//           }
+//       },
+//       "19": {
+//           "id": 19,
+//           "time": "3pm",
+//           "interview": null
+//       },
+//       "20": {
+//           "id": 20,
+//           "time": "4pm",
+//           "interview": null
+//       },
+//       "21": {
+//           "id": 21,
+//           "time": "12pm",
+//           "interview": null
+//       },
+//       "22": {
+//           "id": 22,
+//           "time": "1pm",
+//           "interview": null
+//       },
+//       "23": {
+//           "id": 23,
+//           "time": "2pm",
+//           "interview": {
+//               "student": "Yuko Smith",
+//               "interviewer": 1
+//           }
+//       },
+//       "24": {
+//           "id": 24,
+//           "time": "3pm",
+//           "interview": null
+//       },
+//       "25": {
+//           "id": 25,
+//           "time": "4pm",
+//           "interview": null
+//       }
+//   },
+//   "interviewers": {
+//       "1": {
+//           "id": 1,
+//           "name": "Sylvia Palmer",
+//           "avatar": "https://i.imgur.com/LpaY82x.png"
+//       },
+//       "2": {
+//           "id": 2,
+//           "name": "Tori Malcolm",
+//           "avatar": "https://i.imgur.com/Nmx0Qxo.png"
+//       }
+//   }
+// }
+// const interview = {student: "Jamal Jordan", interviewer: 3}
+
+
+export function getInterview(state, interview) {
+  if (interview) {
+
+    let resultObj = {
+      "student": interview.student,
+      "interviewer": {}
+    };
+
+    const interviewerObject = state.interviewers[interview.interviewer]
+    interviewerObject ?
+      resultObj = { ...resultObj, interviewer: state.interviewers[interview.interviewer] }
+      : resultObj = null;
+    return resultObj;
+  } else {
+    return null
+  }
 }
