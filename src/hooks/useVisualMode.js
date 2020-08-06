5f72ea2af835416bc96eb4c733c1fcb1140d2dff
Kaushik Mehta <kaushikmehta30@gmail.com>
2020-08-06 16:41:42 -0400

    adds replace functionality to useVisualMode

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,7 +1,28 @@
 import { useState } from "react";
 
-export default function useVisualMode(initial){
+export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
-  return {mode};
+  function transition(newMode, replace) {
+    if (replace) {
+      const sliced = history.slice(0, history.length - 1)
+      setHistory([...sliced, newMode])
+    } else {
+      setHistory([...history, newMode])
+    }
+    setMode(newMode)
+  }
+
+
+  function back() {
+    const len = history.length
+    if (len > 1) {
+      const sliced = history.slice(0, history.length - 1)
+      setHistory(sliced)
+      setMode(history[len - 2])
+    }
+  }
+
+  return { mode, transition, back };
 }
